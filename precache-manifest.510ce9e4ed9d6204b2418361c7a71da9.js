f1b61a96bf855a56d02b7f863fbb51c4622eab21
gargNitesh <niteshgarg1312@gmail.com>
2020-08-09 02:36:33 +0530

    Updates

---
+++ precache-manifest.510ce9e4ed9d6204b2418361c7a71da9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cb60eebaa7a954ba76f8b227678234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb981c77f18371c3c001",
+    "url": "/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "aa3b6d96c7ac4e17e9b6",
+    "url": "/static/css/main.391b9a9d.chunk.css"
+  },
+  {
+    "revision": "eb981c77f18371c3c001",
+    "url": "/static/js/2.ed5c39c9.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.ed5c39c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3b6d96c7ac4e17e9b6",
+    "url": "/static/js/main.d53999d9.chunk.js"
+  },
+  {
+    "revision": "cb777255792aa6d50674",
+    "url": "/static/js/runtime-main.348249fa.js"
+  }
+]);
